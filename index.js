d446c8c61103865f4770c04f25dd7f7d0cbbfe06
aliadnankhan <114007060+aliadnankhan@users.noreply.github.com>
2025-11-08 00:07:24 -0500

    V1.03
    connect library assignment 1 (recreate)

--- index.js
+++ index.js
@@ -3,12 +3,33 @@ var connect = require('connect')
 // create app
 var app = connect()
 
-function helloworld(req, res, next) 
+function homepage(req, res, next) 
 {
   res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello World\n')
+  res.end('Home Page\n')
 }
 
-app.use('/hello', helloworld)
+function aboutme(req, res, next) 
+{
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('About Me\n')
+}
+
+function projectpage(req, res, next) 
+{
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('Project Page\n')
+}
+
+function contactus(req, res, next) 
+{
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('Contact Us\n')
+}
+
+app.use('/', homepage)
+app.use('/aboutme', aboutme)
+app.use('/projectpage', projectpage)
+app.use('/contactus', contactus)
 app.listen(3000)
 console.log('Server is running on local host')
