885e895666cdba8404bf73277b643af292f5c078
aliadnankhan <114007060+aliadnankhan@users.noreply.github.com>
2025-11-09 22:21:09 -0500

    V1.06
    Site Setup (with pages)

--- Assignment2/routes/index.js
+++ Assignment2/routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home Page' });
+});
+
+/* GET about me page. */
+router.get('/aboutme', function(req, res, next) {
+  res.render('index', { title: 'About Me' });
+});
+
+/* GET project page. */
+router.get('/project', function(req, res, next) {
+  res.render('index', { title: 'Project Page' });
+});
+
+/* GET contact us page. */
+router.get('/contactus', function(req, res, next) {
+  res.render('index', { title: 'Contact Us' });
 });
 
 module.exports = router;
